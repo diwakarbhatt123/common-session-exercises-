7df4229f3d74a261dcf35ef697b27ac74faad884
Diwakar Bhatt <diwakar.bhatt@tothenew.com>
2016-02-10 14:15:36 +0530

    Added Unit Testing Exercises

---
+++ Unit Testing/test/com/im/test/UserSpec.groovy
@@ -0,0 +1,133 @@
+package com.im.test
+
+import spock.lang.Specification
+import spock.lang.Unroll
+import spock.util.mop.ConfineMetaClassChanges
+
+class UserSpec extends Specification {
+    @Unroll
+    void "Full name valid"() {
+        given:
+        User user = new User()
+        and:
+        user.setFirstName(fname)
+        user.setLastName(lname)
+        expect:
+        user.getFullName() == fullname
+        where:
+        fname     | lname   | fullname
+        null      | null    | null
+        null      | null    | "Diwakar"
+        "Diwakar" | "Bhatt" | "Diwakar Bhatt"
+        123       | 321     | "Diwakar Bhatt"
+    }
+
+    @Unroll
+    void "Display name correct"() {
+        given:
+        User user = new User(firstName: fname, lastName: lname, gender: gender);
+
+        expect:
+
+        user.displayName() == displayname
+
+        where:
+        fname     | lname   | gender | displayname
+        "Diwakar" | "Bhatt" | "Male" | "MrDiwakar Bhatt"
+        null      | null    | "Male" | null
+        "Ajay"    | "Kumar" | null   | "MrAjay Kumar"
+
+    }
+
+    def "Password Valid"(String password) {
+        setup:
+        User user = new User();
+
+
+        expect:
+        user.isValidPassword(password) == true
+
+        where:
+        password << [null, "abc", "abcdefgh"]
+
+    }
+
+    @Unroll
+    def "income group"() {
+        setup:
+        User user = new User(incomePerMonth: incomepm)
+
+        expect:
+        user.getIncomeGroup() == group
+
+        where:
+        incomepm | group
+        1000     | "MiddleClass"
+        6000     | "MiddleClass"
+        20000    | "Higher MiddleClass"
+        10000    | "MiddleClass"
+
+
+    }
+
+    def "Encrypt Password"() {
+        setup:
+        User user = new User()
+        def mockedPasswordEncrypterService = Mock(PasswordEncrypterService)
+        user.passwordEncrypterService = mockedPasswordEncrypterService
+        String result = mockedPasswordEncrypterService.encrypt(_ as String)
+        when:
+        String output = user.encyryptPassword(password)
+
+        then:
+        output == result
+
+        where:
+        password << ["abcd", "abcdefgh"]
+
+    }
+
+    def "Password Reset and Email sent"() {
+        setup:
+        User user = new User()
+        and:
+        def mockEmailService = Mock(EmailService)
+        user.emailService = mockEmailService
+
+        when:
+        user.resetPasswordAndSendEmail()
+
+        then:
+        1 * mockEmailService.sendCancellationEmail(user,_ as String)
+        user.password !="dummy"
+    }
+
+    void "Product Purchased"()
+    {
+        setup:
+        Product product = new Product()
+        User user = new User()
+
+        when:
+        user.purchase(product)
+
+        then:
+        user.purchasedProducts.size()>0
+
+    }
+    void "Product Cancelled"()
+    {
+        setup:
+        Product product = new Product()
+        User user = new User()
+
+        when:
+        user.cancelPurchase(product)
+
+        then:
+        user.purchasedProducts.empty
+
+    }
+
+
+}
